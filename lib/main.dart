0b3400cdd70b336dfa32e075720a4ff079c1a289
Ali W <ali.cast.santos@gmail.com>
2025-04-15 10:16:56 -0300

    chore[sp1-c3]: set up home page sctructure

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,6 @@
 import 'package:flutter/material.dart';
+import 'app.dart';
 
 void main() {
   runApp(const CompareCambioApp());
-}
-
-class CompareCambioApp extends StatelessWidget {
-  const CompareCambioApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Compare Câmbio',
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Center(child: Text('Compare Câmbio')),
-      ),
-    );
-  }
 }
